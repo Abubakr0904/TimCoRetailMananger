8a691de5af2f4207a259d87d40efdcb77328ec1d
RCCSilva <rafael.coelho.x@gmail.com>
2020-12-16 13:54:10 -0300

    Add User controller to API

---
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public IList<UserModel> GetUserById(string id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = id };
+
+            var result = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "TRMData");
+
+            return result;
+        }
+    }
+}
