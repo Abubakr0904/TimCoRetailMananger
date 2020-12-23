8834a194d082a77dfcf1d4f2c2fb5d5b71a90cec
RCCSilva <rafael.coelho.x@gmail.com>
2020-12-23 16:57:36 -0300

    Replace container reference in ShellViewModel

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -11,24 +11,20 @@ namespace TRMDesktopUI.ViewModels
     public class ShellViewModel: Conductor<object>, IHandle<LogOnEventModel>
     {
         private IEventAggregator _eventAggregator;
-        private SimpleContainer _container;
 
         public ShellViewModel(
-            SimpleContainer container,
             IEventAggregator eventAggregator
             )
         {
-            _container = container;
-
             _eventAggregator = eventAggregator;
             _eventAggregator.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEventModel message)
         {
-            ActivateItem(_container.GetInstance<SalesViewModel>());
+            ActivateItem(IoC.Get<SalesViewModel>());
         }
     }
 }
